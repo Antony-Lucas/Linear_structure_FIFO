6c77b1bac19b98c99005c1bbc3e5d36d5e709680
Antony Lucas <54651622+Antony-Lucas@users.noreply.github.com>
2022-07-19 18:54:57 -0300

    Update data_manager.dart

--- lib/widgets/data_manager.dart
+++ lib/widgets/data_manager.dart
@@ -1,8 +1,5 @@
 class DataManager {
-  //Duas strings para armazenar textos.
   String title;
   String description;
-  /*construtor da classe que referencia as variáveis 
-  acima e indica que os parâmetros são obrigatórios*/
   DataManager({required this.title, required this.description});
 }
